a9dafe758f7bcf12e24ab35fc1c2586e05796b24
Ngo Quang Thang <ngoquangthang2003@gmail.com>
2024-09-08 19:31:55 +0700

    Init commit

---
+++ app/root.tsx
@@ -0,0 +1,56 @@
+import {
+  Links,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
+import "./tailwind.css";
+import { useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { LinksFunction } from "@remix-run/node";
+import styles from "./tailwind.css?url";
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: styles, as: "style" },
+  { rel: "stylesheet", href: "/css/tailwind1.css" },
+];
+
+export default function App() {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 60 * 1000,
+          },
+        },
+      })
+  );
+
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link href="/images/favicon.ico" rel="icon" />
+        <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={"anonymous"}></link>
+        <link href="https://fonts.googleapis.com/css2?family=Inter:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet"></link>
+        <title>EgoFlask</title>
+        <meta name="description" content="Ego Flask" />
+        <Meta />
+        <Links />
+      </head>
+      <QueryClientProvider client={queryClient}>
+        <body>
+          <div id="wrapper">
+            <Outlet />
+          </div>
+          <ScrollRestoration />
+          <Scripts />
+        </body>
+      </QueryClientProvider>
+    </html>
+  );
+}
