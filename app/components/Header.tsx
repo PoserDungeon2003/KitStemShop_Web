64c5b74f7eacbfe4821b03bf324c95f652342261
Ngo Quang Thang <ngoquangthang2003@gmail.com>
2024-10-10 16:16:21 +0700

    Fix register form

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -53,7 +53,7 @@ export const Header = () => {
                   <item.icon className="text-2xl" />
                   {item.length && (
                     <div
-                      className="absolute -right-2 -top-1 w-5 h-5 rounded-full flex items-center justify-center bg-primary text-white text-xs">
+                      className="absolute -right-2 -top-1 w-5 h-5 rounded-full flex items-center justify-center bg-primary !text-white text-xs">
                       {item.length}
                     </div>
                   )}
--- app/routes/register.tsx
+++ app/routes/register.tsx
@@ -1,41 +1,99 @@
+import { yupResolver } from "@hookform/resolvers/yup";
 import { Link } from "@remix-run/react";
+import { useForm } from "react-hook-form";
 import { FaFacebook, FaGoogle } from "react-icons/fa6";
+import { object, ref, string, InferType, number } from "yup";
+
+const schema = object({
+  fullName: string().required('Full name is required').trim(),
+  userName: string().required('Full name is required').trim(),
+  passwordHash: string()
+    .min(6, 'Mật khẩu phải có ít nhất 6 ký tự')
+    .max(25, "Mật khẩu không được quá 25 ký tự")
+    .matches(/[A-Z]/, 'Mật khẩu phải có ít nhất một chữ in hoa')
+    .matches(/[!@#$%^&*(),.?":{}|<>]/, 'Mật khẩu phải có ít nhất một ký tự đặc biệt')
+    .required()
+    .trim(),
+  confirmPassword: string()
+    .oneOf([ref('passwordHash')], 'Mật khẩu xác nhận không khớp')
+    .required(),
+  email: string().email('Invalid email').required('Email is required').trim(),
+  address: string().required('Address is required').trim(),
+  phone: number().required('Phone number is required').typeError('Phone number is required').positive('Phone number is required').integer('Phone number is required'),
+})
+
+const resolver = yupResolver(schema)
+
+type LoginForm = InferType<typeof schema>
 
 export default function Register() {
+  const { register, formState: { errors, isSubmitting }, handleSubmit, setError, watch } = useForm<LoginForm>({
+    mode: 'onChange',
+    resolver,
+  })
+  const onSubmit = (data: LoginForm) => {
+
+  }
+
   return (
     <div className="contain py-16">
       <div className="max-w-lg mx-auto shadow px-6 py-7 rounded overflow-hidden">
         <h2 className="text-2xl uppercase font-medium mb-1">Create an account</h2>
         <p className="text-gray-600 mb-6 text-sm">Register for new customer</p>
 
-        <form method="post">
+        <form method="post" onSubmit={handleSubmit(onSubmit)}>
           <div className="space-y-2">
             <div>
               <label htmlFor="name" className="text-gray-600 mb-2 block">Full Name</label>
               <input
                 type="text"
-                name="name"
-                id="name"
+                {...register('fullName')}
                 className="block w-full border border-gray-300 px-4 py-3 text-gray-600 text-sm rounded focus:ring-0 focus:border-primary placeholder-gray-400"
                 placeholder="fulan fulana"
+              />
+            </div>
+            <div>
+              <label htmlFor="name" className="text-gray-600 mb-2 block">Username</label>
+              <input
+                type="text"
+                {...register('userName')}
+                className="block w-full border border-gray-300 px-4 py-3 text-gray-600 text-sm rounded focus:ring-0 focus:border-primary placeholder-gray-400"
+                placeholder="username"
               />
             </div>
             <div>
               <label htmlFor="email" className="text-gray-600 mb-2 block">Email address</label>
               <input
                 type="email"
-                name="email"
-                id="email"
+                {...register('email')}
                 className="block w-full border border-gray-300 px-4 py-3 text-gray-600 text-sm rounded focus:ring-0 focus:border-primary placeholder-gray-400"
                 placeholder="youremail.@domain.com"
+              />
+            </div>
+            <div>
+              <label htmlFor="address" className="text-gray-600 mb-2 block">Address</label>
+              <input
+                type="text"
+                {...register('address')}
+                className="block w-full border border-gray-300 px-4 py-3 text-gray-600 text-sm rounded focus:ring-0 focus:border-primary placeholder-gray-400"
+                placeholder="145 Đường Nguyễn Cơ Thạch"
+              />
+            </div>
+            <div>
+              <label htmlFor="phone" className="text-gray-600 mb-2 block">Phone number</label>
+              <input
+                type="tel"
+                inputMode="tel"
+                {...register('phone')}
+                className="block w-full border border-gray-300 px-4 py-3 text-gray-600 text-sm rounded focus:ring-0 focus:border-primary placeholder-gray-400"
+                placeholder="0906"
               />
             </div>
             <div>
               <label htmlFor="password" className="text-gray-600 mb-2 block">Password</label>
               <input
                 type="password"
-                name="password"
-                id="password"
+                {...register('passwordHash')}
                 className="block w-full border border-gray-300 px-4 py-3 text-gray-600 text-sm rounded focus:ring-0 focus:border-primary placeholder-gray-400"
                 placeholder="*******"
               />
@@ -44,8 +102,7 @@ export default function Register() {
               <label htmlFor="confirm" className="text-gray-600 mb-2 block">Confirm password</label>
               <input
                 type="password"
-                name="confirm"
-                id="confirm"
+                {...register('confirmPassword')}
                 className="block w-full border border-gray-300 px-4 py-3 text-gray-600 text-sm rounded focus:ring-0 focus:border-primary placeholder-gray-400"
                 placeholder="*******"
               />
