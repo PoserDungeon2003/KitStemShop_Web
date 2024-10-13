96326331efde5191d36b4b2ffca35ec362192d54
tungske <trinhsontung2410@gmail.com>
2024-10-13 22:22:58 +0700

    Update supportrequest.ts

--- app/data/supportrequest.ts
+++ app/data/supportrequest.ts
@@ -5,24 +5,28 @@ export async function getSupportRequest(): Promise<SupportRequest> {
     return await request.get(`${BASE_URL}/api/SupportRequest`);
 }
 
+// export async function createSupportRequest(data: SupportRequest): Promise<any> {
+//   try {
+//     const response = await fetch(`${BASE_URL}/api/SupportRequest`, {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
+//       body: JSON.stringify(data),  // Send the request data as JSON
+//     });
+
+//     if (!response.ok) {
+//       throw new Error(`HTTP error! status: ${response.status}`);
+//     }
+
+//     const resData = await response.json();  // Parse the response JSON
+//     return resData;  // Return the parsed response
+//   } catch (error) {
+//     console.error("Error creating support request:", error);
+//     throw error;  // Rethrow the error so it can be caught in the calling function
+//   }
+// }
+
 export async function createSupportRequest(data: SupportRequest): Promise<any> {
-  try {
-    const response = await fetch(`${BASE_URL}/api/SupportRequest`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(data),  // Send the request data as JSON
-    });
-
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-
-    const resData = await response.json();  // Parse the response JSON
-    return resData;  // Return the parsed response
-  } catch (error) {
-    console.error("Error creating support request:", error);
-    throw error;  // Rethrow the error so it can be caught in the calling function
-  }
+    return await request.post(`${BASE_URL}/api/SupportRequest`, data);
 }
