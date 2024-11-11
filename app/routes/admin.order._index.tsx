03c3c339fd213b2854ce00f8815ad8e04698e854
Ngo Quang Thang <ngoquangthang2003@gmail.com>
2024-11-12 01:12:38 +0700

    Add search order

--- app/routes/admin.order._index.tsx
+++ app/routes/admin.order._index.tsx
@@ -1,11 +1,15 @@
 import { useNavigate } from "@remix-run/react";
-import { Table, TableProps, Tag, Tooltip } from "antd"
-import { format } from "date-fns";
+import { Input, Table, TableProps, Tag, Tooltip } from "antd"
+import { format, isWithinInterval, parseISO } from "date-fns";
 import _ from "lodash";
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
+import { FaMagnifyingGlass } from "react-icons/fa6";
 import { IoEye } from "react-icons/io5";
 import { formatMoney } from "~/components/utils";
 import { KitItem, Order, useGetAllCombos, useGetAllKits, useGetAllOrders, useGetProfile } from "~/data"
+import { DatePicker, Space } from 'antd';
+
+const { RangePicker } = DatePicker;
 
 export const handle = {
   hideFooter: true,
@@ -18,12 +22,31 @@ export default function AdminOrder() {
   const profile = useGetProfile();
   const orders = useGetAllOrders(profile.data?.user?.token || "");
   const navigate = useNavigate();
+  const [search, setSearch] = useState<string>("");
+  const [dateRange, setDateRange] = useState<string[] | undefined>(undefined);
 
   const datasource = useMemo(() => {
     return _(orders.data?.data)
+      .filter(it => {
+        if (search.length > 0) {
+          return it.orderId.toString().toLowerCase().trim().includes(search.toLowerCase().trim());
+        }
+        return true;
+      })
+      .filter(it => {
+        if (dateRange && dateRange[0]?.length > 0 && dateRange[1]?.length > 0) {
+          const startDate = parseISO(dateRange[0]);
+          const endDate = parseISO(dateRange[1]);
+          return isWithinInterval(it.orderDate, {
+            start: startDate,
+            end: endDate
+          })
+        }
+        return true;
+      })
       .orderBy(it => it.orderDate, "desc")
       .value();
-  }, [orders.data?.data]);
+  }, [orders.data?.data, search, dateRange]);
 
   const columns: TableProps<Order>['columns'] = [
     {
@@ -32,7 +55,7 @@ export default function AdminOrder() {
       key: 'orderId',
     },
     {
-      title: 'Totoal Amount',
+      title: 'Total Amount',
       dataIndex: 'totalAmount',
       key: 'totalAmount',
       render: (text: number) => <span>{formatMoney(text)}</span>,
@@ -68,9 +91,26 @@ export default function AdminOrder() {
       render: (text: string) => format(text, "HH:mm:ss dd/MM/yyyy"),
     },
   ];
-  
+
   return (
     <div>
+      <div className="flex flex-col gap-4">
+        <Input
+          onChange={(e) => {
+            setSearch(e.target.value);
+          }}
+          className="header-search w-1/4"
+          placeholder="Type here..."
+          prefix={<FaMagnifyingGlass />}
+        />
+        <Space direction="vertical" size={12}>
+          <RangePicker onCalendarChange={(dates, dateStrings) => {
+            setDateRange(dateStrings);
+            console.log(dateStrings);
+            
+          }}/>
+        </Space>
+      </div>
       <Table loading={orders.isLoading} columns={columns} dataSource={datasource}>
       </Table>
     </div>
