ec061c5f54c58977ddf2dd5c6b08185e5612e198
tungske <trinhsontung2410@gmail.com>
2024-11-11 19:59:44 +0700

    remove login gg and facebook and remove read more in blog

--- app/routes/blog.tsx
+++ app/routes/blog.tsx
@@ -30,9 +30,9 @@ export default function BlogList() {
             </a>
             By {blog.user || "Annymous"}
             <p className="font-normal text-gray-700 mb-3 line-clamp-3">{blog.content}</p>
-            <a className="!text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 text-center inline-flex items-center" href="#">
+            {/* <a className="!text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 text-center inline-flex items-center" href="#">
               Read more
-            </a>
+            </a> */}
           </div>
         </div>
       ))}
--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -115,7 +115,7 @@ export default function Login() {
           <div className="absolute left-0 top-3 w-full border-b-2 border-gray-200"></div>
         </div>
         <div className="mt-4 flex gap-4">
-          <Link
+          {/* <Link
             to="#"
             className="w-1/2 flex flex-1 items-center justify-center gap-2 py-2 text-center text-white bg-blue-800 rounded uppercase font-roboto font-medium text-sm hover:bg-blue-700"
           >
@@ -128,7 +128,7 @@ export default function Login() {
           >
             <FaGoogle className="aspect-square w-5 h-5" />
             <span>Google</span>
-          </Link>
+          </Link> */}
         </div>
         {/* ./Login with */}
 
