b5fe9909e199e98b1b35b1f6c17d8d27ba3431df
Ngo Quang Thang <ngoquangthang2003@gmail.com>
2024-11-15 15:26:25 +0700

    Clear cart

--- app/routes/checkout-success.tsx
+++ app/routes/checkout-success.tsx
@@ -1,34 +1,38 @@
 import { json, useLocation } from "@remix-run/react";
 import { useQueryClient } from "@tanstack/react-query";
+import { message } from "antd";
+import { useEffect } from "react";
 import { FaCircleCheck, FaCircleXmark } from "react-icons/fa6";
 import { Link, LoaderFunctionArgs, redirect, useNavigate } from "react-router-dom";
-import { clearCart } from "~/data";
+import { clearCart, useGetProfile } from "~/data";
 import { authenticator } from "~/services/auth.server";
 
 export async function loader({ request }: LoaderFunctionArgs) {
-  let user = await authenticator.isAuthenticated(request);
-  console.log('user', user);
-  
-  if (!user) return redirect("/");
-
-  try {
-    const response = await clearCart(user?.token || "");
-    if (response) {
-      return json({}, { status: 200 });
-    }
-    return json({}, { status: 400 });
-  } catch (error) {
-    return redirect(`/checkout?message=${encodeURIComponent('Giao dịch thất bại')}&type=error`);
-  }
 }
 
 export default function CheckoutSuccess() {
+  const profile = useGetProfile();
   const location = useLocation();
   const queryParams = new URLSearchParams(location.search);
-  const message = queryParams.get("message");
+  const messageParams = queryParams.get("message");
   const code = queryParams.get("code");
   const queryClient = useQueryClient();
   const navigate = useNavigate();
+
+  const clearCartItems = async () => {
+    try {
+      const response = await clearCart(profile.data?.user?.token || "");
+      if (response) {
+        message.success("Thanh toán thành công");
+      }
+    } catch (error) {
+      return navigate(`/checkout?message=${encodeURIComponent('Giao dịch thất bại')}&type=error`);
+    }
+  }
+
+  useEffect(() => {
+    clearCartItems();
+  }, [])
 
   return (
     <main className="mt-20">
@@ -36,7 +40,7 @@ export default function CheckoutSuccess() {
         {code === '00' ?
           <FaCircleCheck className="w-20 h-20 text-green-500" /> :
           <FaCircleXmark className="w-20 h-20 text-red-500" />}
-        <h1 className="text-xl">{message || 'Giao dịch thất bại'}</h1>
+        <h1 className="text-xl">{messageParams || 'Giao dịch thất bại'}</h1>
         {code == '00' ? (
           <div className="text-sm cursor-pointer" onClick={() => {
             queryClient.invalidateQueries({
